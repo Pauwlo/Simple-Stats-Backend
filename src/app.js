a63e523794aea6214eaea09bc17622f4d9728d9f
Pauwlo <dev@pauwlo.fr>
2021-09-07 00:26:54 +0200

    Remove body-parser package, move to src/

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser')
 const router = require('./router')
 
 const PORT = process.env.PORT
@@ -9,7 +8,7 @@ const app = new express()
 app.set('port', process.env.PORT)
 app.disable('x-powered-by')
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use('/', router)
 
 app.listen(PORT, () => {
